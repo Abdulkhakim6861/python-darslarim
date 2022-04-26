3705c285f48be447373c4d7fd7a862eccc771886
Abdulkhakim6861 <104383980+Abdulkhakim6861@users.noreply.github.com>
2022-04-26 13:43:38 +0500

    yangi-o'zgarishlar
    12- darsdan bazi malumotlar q'shildi.

---
+++ 12-dars/hello-word.py
@@ -0,0 +1,21 @@
+"""
+12 - dars
+26.04.2022
+
+mehmonlar = ['Hasan', 'Husan', 'Ali', 'Vali', 'jasur', ' Joxa']
+for mehmon in mehmonlar:
+  print("salom", mehmon)  
+  print("Hayr", mehmon)
+
+  
+mehmonlar = ['Hasan', 'Husan', 'Ali', 'Vali', 'jasur', ' Joxa']
+for mehmon in mehmonlar:
+sonlar = list(range(11))
+sonlar_kvadrati= []
+for son in sonlar:
+    #sonlar_kvadrati.append(son**2)
+#print(sonlar)
+#print(sonlar_kvadrati)
+dostlar = []
+####
+sonlar = range(11)
