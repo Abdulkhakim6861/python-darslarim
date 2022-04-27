9182689f9464a2c9ec2f9e74a7881a48aaa733fd
Abdulkhakim6861 <104383980+Abdulkhakim6861@users.noreply.github.com>
2022-04-27 15:47:28 +0500

    Create 15- dars Lug'at bilan ishlash 2.py

---
+++ 15 dars/15- dars Lug'at bilan ishlash 2.py
@@ -0,0 +1,35 @@
+#talaba_0 = {
+  #  'familiya':'alijonov',
+ #   'ism':'aziz',
+  #  'fakultet' : 'pedagogika',
+  #  'kurs': 4
+  #  }
+#print(talaba_0.items())    
+#for kalit, qiymat in talaba_0.items():
+  #  print(f"Kalit: {kalit}")
+ #   print(f"Qiymat: {qiymat} \n")
+#print(talaba_0.items())
+
+#print(talaba_0.items())
+#for kalit, qiymat in talaba_0.items():
+ #   print(f"Kalit : {kalit}")
+  #  print(f"Qiymat : {qiymat} \n ")
+#telefonlar = {  
+#'ali': 'iphone 13 pro max',
+#'valisher': 'samsung s 21 ultra',
+#'sherbek': 'mi 11 pro',
+#'jonibek' : 'nokia 6233',
+#}
+#for k, q in telefonlar.items():
+ #   print(f"{k.title()}ning telefoni {q}")
+mahsulotlar = {
+     'olma':20000,
+     'anor':25000,
+     'hurmo':29000,
+     'banan':21000
+     }
+print(mahsulotlar.keys())
+
+print("Do'kondagi mahsulotlar:")
+for mahsulot in mahsulotlar.keys():
+    print(mahsulot.title())
