e2b9365645cff98f7c4abe03ffc6b86043a501fd
Abdulkhakim6861 <104383980+Abdulkhakim6861@users.noreply.github.com>
2022-04-28 14:02:47 +0500

    Update 15- dars Lug'at bilan ishlash 2.py

--- 15 dars/15- dars Lug'at bilan ishlash 2.py
+++ 15 dars/15- dars Lug'at bilan ishlash 2.py
@@ -22,14 +22,47 @@
 #}
 #for k, q in telefonlar.items():
  #   print(f"{k.title()}ning telefoni {q}")
-mahsulotlar = {
-     'olma':20000,
-     'anor':25000,
-     'hurmo':29000,
-     'banan':21000
-     }
-print(mahsulotlar.keys())
+#mahsulotlar = {
+   #  'olma':20000,
+    # 'anor':25000,
+  #  'banan':21000
+    # }
+#print(mahsulotlar.keys())
 
-print("Do'kondagi mahsulotlar:")
-for mahsulot in mahsulotlar.keys():
-    print(mahsulot.title())
+#print("Do'kondagi mahsulotlar:")
+#for mahsulot in mahsulotlar.keys():
+    #print(mahsulot.title())
+#bozorlik = ['uzum', 'anor', 'olma', 'anjir', 'banan']
+#for mahsulot in mahsulotlar:
+ #   if mahsulot in bozorlik:
+ #       print(f"{mahsulot.title()} {mahsulotlar[mahsulot]} so'm")
+#for buyum in bozorlik:
+#    if buyum not in mahsulotlar:
+ #       print(f" Iltimos do'koningizga {buyum} ham olib keling")
+#print("Do'koningizdagi mahsulotlar:")
+#or mahsulot in sorted(mahsulotlar):
+  #  print(mahsulot.title())
+#telefonlar = {  
+#'ali': 'iphone 13 pro max',
+#'valisher': 'samsung s 21 ultra',
+#'sherbek': 'mi 11 pro',
+#'jonibek' : 'nokia 6233',
+#}
+#print(telefonlar.values()))
+#print("Foydalanuvchilar quydagi telefonlar ishlatishadi."
+#for tel in telefonlar.values():
+ #   print(tel)
+#telefonlar = {  
+#'ali': 'iphone 13 pro max',
+#'valisher': 'samsung s 21 ultra',
+#'sherbek': 'mi 11 pro',
+#'jonibek' : 'nokia 6233',
+#'umar' : 'huawei p 30',
+#'jasur' : 'iphone x',
+#'usmon': 'samsung s 21 ultra',
+#'mariyam' : 'iphone x'
+#}
+#print("Foydalanuvchilar quydagi telefonlar ishlatishadi.")
+#for tel in set(telefonlar.values()):
+ #   print(tel)
+toys = {'teddy' , 'lamp', 'car', 'doll', 'ball', 'car', 'lamp', 'bear'}
