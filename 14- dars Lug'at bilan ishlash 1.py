cc70c5b7bbf77feb9b9b13c689c58214038dd8e0
Abdulkhakim6861 <104383980+Abdulkhakim6861@users.noreply.github.com>
2022-05-12 13:16:54 +0500

    yangi fail qo'shikdi
    bu darsda nesting haqida malumotlar berilgan

--- 14- dars Lug'at bilan ishlash 1.py
+++ 14- dars Lug'at bilan ishlash 1.py
@@ -1,32 +1,35 @@
-#car_0 = {'model': 'ferrari', 'rang': 'qizil'}
-#print(car_0['model'])
-#print(car_0['rang'])
+#talaba_0 = {
+  #  'familiya':'alijonov',
+ #   'ism':'aziz',
+  #  'fakultet' : 'pedagogika',
+  #  'kurs': 4
+  #  }
+#print(talaba_0.items())    
+#for kalit, qiymat in talaba_0.items():
+  #  print(f"Kalit: {kalit}")
+ #   print(f"Qiymat: {qiymat} \n")
+#print(talaba_0.items())
 
-#en_uz = {'apple':'olma', 'apricot':"o'rik", 'banana':'banan'}
-#print(en_uz['apple'])
-#mevalar = {'olma':10000, 'tarvuz':9000, 'banan':21000}
-#print(mevalar['banan'])
-#talaba_0 = {'ism':'olim murodov', 'yosh':20, 't_yil':2000}
-#print(f"{talaba_0['ism'].title()},\
-     #{talaba_0['t_yil']}-yilda tug'ulgan,\
-        # {talaba_0['yosh']} yoshda")
-#talaba_0['fakultet'] = 'pedagogika'
-#talaba_1 = {}
-#talaba_1['ism'] = 'Abdullah'
-#talaba_1['yosh'] = 20
-#talaba_1['kurs'] = 3
-#print(talaba_1)
-#print(f"Talaba {talaba_1['ism'].title()} {talaba_1['kurs']}-kursda")
-#talaba_1['kurs'] = 4
-#print(f"Talaba {talaba_1['ism'].title()} {talaba_1['kurs']}-kursda")
-#talaba_0 = {'ism':'olim murodov', 'yosh':20, 't_yil':2000}
-#del talaba_0['yosh']
-#talaba_0['yosh'] = 20
-telefonlar = {  
-   'ali': 'iphone 13 pro max',
-    'valisher': 'samsung s 21 ultra',
-    'sherbek': 'mi 11 pro',
-    'jonibek': 'nokia 6233'
-    }
-phone = telefonlar.get("hasan", 'Bunday ism mavjud emas')
-print(phone)
+#print(talaba_0.items())
+#for kalit, qiymat in talaba_0.items():
+ #   print(f"Kalit : {kalit}")
+  #  print(f"Qiymat : {qiymat} \n ")
+#telefonlar = {  
+#'ali': 'iphone 13 pro max',
+#'valisher': 'samsung s 21 ultra',
+#'sherbek': 'mi 11 pro',
+#'jonibek' : 'nokia 6233',
+#}
+#for k, q in telefonlar.items():
+ #   print(f"{k.title()}ning telefoni {q}")
+mahsulotlar = {
+     'olma':20000,
+     'anor':25000,
+     'hurmo':29000,
+     'banan':21000
+     }
+print(mahsulotlar.keys())
+
+print("Do'kondagi mahsulotlar:")
+for mahsulot in mahsulotlar.keys():
+    print(mahsulot.title())
